529bffcdff6bea1c287a900751d6935475064bea
mThanuj <thanujmullaguri1825@gmail.com>
2024-09-25 17:36:44 +0530

    mThanuj

--- src/Day_01_Bit_Manipulation/Problems/Divide_Without_Division_Or_Multiplication.java
+++ src/Day_01_Bit_Manipulation/Problems/Divide_Without_Division_Or_Multiplication.java
@@ -12,6 +12,31 @@ public class Divide_Without_Division_Or_Multiplication {
 			int dividend,
 			int divisor
 	) {
+		if (dividend == divisor) {
+			return 1;
+		}
 
+		boolean sign = true;
+		if (dividend >= 0 && divisor < 0) {
+			sign = false;
+		} else if (dividend < 0 && divisor > 0) {
+			sign = false;
+		}
+
+		dividend = Math.abs(dividend);
+		divisor = Math.abs(divisor);
+
+		int res = 0;
+		while (dividend >= divisor) {
+			int count = 0;
+			while (dividend >= (divisor << (count + 1))) {
+				count += 1;
+			}
+
+			res += 1 << count;
+			dividend -= (divisor << (count));
+		}
+
+		return (sign) ? res : (-1 * res);
 	}
 }
